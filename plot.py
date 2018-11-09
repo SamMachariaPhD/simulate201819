8f9ed49410bcf85900b99c114c8b621ab7ad8aa3
sirmaxford <sirmaxford@gmail.com>
2018-11-09 18:48:21 +0900

    Fixed the on-plot vel. text off positioning bug

--- plot.py
+++ plot.py
@@ -23,6 +23,12 @@ df_load = pd.read_csv('TipXY_A001.txt', names=columns, delim_whitespace=True)
 df_nice = df_load.drop(['time'], axis=1)
 df = df_nice.iloc[0::9, :] #pick every 9th row starting from the first
 df.to_csv(results_dir+'/SkippedTipXY_A001.csv', encoding='utf-8', index=False)
+x_max = df['x_tip'].max(); x_min = df['x_tip'].min()
+y_max = df['y_tip'].max(); y_min = df['y_tip'].min()
+x1 = (0.01*(x_max-x_min))+x_min
+y1 = (0.90*(y_max-y_min))+y_min
+x2 = (0.15*(x_max-x_min))+x_min #+(0.43*(x_max/x_min))
+y2 = (0.85*(y_max-y_min))+y_min #0.5
 
 Dx_tip = np.diff(df['x_tip']); Dy_tip = np.diff(df['y_tip'])
 DD=np.sqrt((Dx_tip**2)+(Dy_tip**2))
@@ -32,8 +38,8 @@ np.savetxt(results_dir+'/v.csv', v, delimiter=',')
 vSD=np.sum(((v-Av_vel)**2)/(np.size(v)-1)); vSD=np.sqrt(vSD)
 
 plt.figure(figsize=(10,8))
-plt.text(6, 8, 'Av_Vel = ', fontdict=font); plt.text(8, 8, '%.5f'%Av_vel, fontdict=font)
-plt.text(6, 7, 'Vel_SD = ', fontdict=font); plt.text(8, 7, '%.5f'%vSD, fontdict=font)
+plt.text(x1, y1, 'Av_Vel = ', fontdict=font); plt.text(x2, y1, '%.5f'%Av_vel, fontdict=font)
+plt.text(x1, y2, 'Vel_SD = ', fontdict=font); plt.text(x2, y2, '%.5f'%vSD, fontdict=font)
 plt.plot(df['x_tip'],df['y_tip'], label='Filament', color='green', marker='o', linestyle='dashed', linewidth=2, markersize=7)
 plt.xlabel('X Tip', fontdict=font); plt.ylabel('Y Tip', fontdict=font)
 plt.title('Actin Filament Movement'); plt.legend(loc='upper left'); plt.grid()
