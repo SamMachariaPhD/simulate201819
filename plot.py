9b752cdcffe55cd3c8ce459528c53d094150b706
sirmaxford <sirmaxford@gmail.com>
2018-11-22 12:24:14 +0900

    polished vel and plot accuracy

--- plot.py
+++ plot.py
@@ -21,14 +21,14 @@ else:
 columns = ['time', 'x_tip', 'y_tip']; DtFile=0.01
 df_load = pd.read_csv('TipXY_A001.txt', names=columns, delim_whitespace=True)
 df_nice = df_load.drop(['time'], axis=1)
-df = df_nice.iloc[0::9, :] #pick every 9th row starting from the first
+df = df_nice.iloc[0::10, :] #pick every 9th row starting from the first
 df.to_csv(results_dir+'/SkippedTipXY_A001.csv', encoding='utf-8', index=False)
 x_max = df['x_tip'].max(); x_min = df['x_tip'].min()
 y_max = df['y_tip'].max(); y_min = df['y_tip'].min()
 x1 = (0.01*(x_max-x_min))+x_min
 y1 = (0.90*(y_max-y_min))+y_min
-x2 = (0.15*(x_max-x_min))+x_min #+(0.43*(x_max/x_min))
-y2 = (0.85*(y_max-y_min))+y_min #0.5
+x2 = (0.15*(x_max-x_min))+x_min 
+y2 = (0.85*(y_max-y_min))+y_min 
 
 Dx_tip = np.diff(df['x_tip']); Dy_tip = np.diff(df['y_tip'])
 DD=np.sqrt((Dx_tip**2)+(Dy_tip**2))
@@ -40,8 +40,8 @@ vSD=np.sum(((v-Av_vel)**2)/(np.size(v)-1)); vSD=np.sqrt(vSD)
 plt.figure(figsize=(10,8))
 plt.text(x1, y1, 'Av_Vel = ', fontdict=font); plt.text(x2, y1, '%.5f'%Av_vel, fontdict=font)
 plt.text(x1, y2, 'Vel_SD = ', fontdict=font); plt.text(x2, y2, '%.5f'%vSD, fontdict=font)
-plt.plot(df['x_tip'],df['y_tip'], label='Filament', color='green', marker='o', linestyle='dashed', linewidth=2, markersize=7)
-plt.xlabel('X Tip', fontdict=font); plt.ylabel('Y Tip', fontdict=font)
+plt.plot(df['x_tip'],df['y_tip'], label='Leading tip', color='green', marker='o', linestyle='dashed', linewidth=2, markersize=7)
+plt.xlabel('X', fontdict=font); plt.ylabel('Y', fontdict=font)
 plt.title('Actin Filament Movement '+plot_name); plt.legend(loc='upper left'); plt.grid()
 plt.savefig(results_dir+'/'+plot_name+'.svg', format='svg', dpi=1200)
 plt.savefig(results_dir+'/'+plot_name+'.png', format='png')
