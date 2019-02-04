78461362409adbf36ecd5b78ae3905ae94286cb7
sirmaxford <sirmaxford@gmail.com>
2019-02-04 12:57:15 +0900

    2 seeds complete analysis

--- grindingdata.py
+++ grindingdata.py
@@ -1,9 +1,10 @@
 #After simulation, get inside each folder.
 #Extract the binding motor number.
 #Extract speed and its deviation.
+#Sort data and do instructed analysis
 #Sam. Prof. Nitta Lab.
 
-import os,glob,csv
+import os,glob,csv,time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,11 +21,13 @@ filename = 'grinding_data'
 dirlist = glob.glob(current_path+'/*/') #[name for name in os.listdir(".") if os.path.isdir(name)]
 dirlist = sorted(dirlist, key=lambda x:x[-30:])
 
-dt = 0.01 # change in time between rows
-conf_names = ['time','x','y','z']
-rownames = ['time','bmotors','vel']
+dt = 0.01 # change in stime between rows
+conf_names = ['stime','x','y','z']
+rownames = ['stime','bmotors','vel']
+columnslt = ['stime', 'x_tip', 'y_tip']
 avsd = ['avSpeed','std_dev']
 binding_motor = 0
+no = 1
 
 try:
     os.remove(filename+'.csv') # if program run twice, remove the previously saved file.
@@ -34,6 +37,9 @@ try:
     os.remove('bmd07.csv'); os.remove('bmd08.csv'); os.remove('bmd09.csv'); os.remove('bmd10.csv')
 except (OSError, RuntimeError, TypeError, NameError):
     pass
+
+print('\n ==> Now mining data ...\n')
+tic = time.time()
 
 for i in dirlist:
     os.chdir(i)
@@ -57,7 +63,7 @@ for i in dirlist:
     conf = pd.read_csv('Conformation_A001.txt', names=conf_names, delim_whitespace=True)
     conf = conf.iloc[0::13,:] # jump 13 starting from the first index
     conf = conf.reset_index(drop=True) # reset index to have a fresh dataframe for leading tip
-    conf = conf.iloc[0::10, :] # jump every 10 -- coz recommended time is 0.01*10 sec
+    conf = conf.iloc[0::10, :] # jump every 10 -- coz recommended stime is 0.01*10 sec
     conf = conf[['x','y']]
     Dx_tip = np.diff(conf['x']); Dy_tip = np.diff(conf['y'])
     DD=np.sqrt((Dx_tip**2)+(Dy_tip**2))
@@ -67,11 +73,27 @@ for i in dirlist:
     v1 = v.reshape(30,1)
     #print(av_sd)
     v1 = pd.DataFrame(v1)
+    #----------------------------
+    df_load = pd.read_csv('TipXY_A001.txt', names=columnslt, delim_whitespace=True)
+    df_nice = df_load.drop(['stime'], axis=1)
+    dflt = df_nice.iloc[0::10, :] #pick every 9th row starting from the first
+    Dx_tip = np.diff(dflt['x_tip']); Dy_tip = np.diff(dflt['y_tip'])
+    DDlt=np.sqrt((Dx_tip**2)+(Dy_tip**2))
+    vlt=DDlt/(10*dt); Av_vellt = np.mean(vlt)
+    vSDlt=np.sum(((vlt-Av_vellt)**2)/(np.size(vlt)-1)); vSDlt=np.sqrt(vSDlt)
+    plt.figure(figsize=(10,6))
+    plt.plot(dflt['x_tip'],dflt['y_tip'], label='Leading tip', color='green', marker='o', linestyle='dashed') #, linewidth=2, markersize=7
+    plt.xlabel('X', fontdict=font); plt.ylabel('Y', fontdict=font)
+    plt.title('Actin Filament Leading Tip Movement'); plt.legend(loc='upper left')
     os.chdir(current_path)
-    time = np.around(np.linspace(0,3,30,endpoint=True), decimals=1)
-    tym = pd.DataFrame(time)
+    plt.savefig('leadingtip'+str(no)+'.svg',bbox_inches='tight', format='svg', dpi=500)
+    plt.close()
+    no = round(no+1,1)
+    #----------------------------
+    stime = np.around(np.linspace(0,3,30,endpoint=True), decimals=1)
+    tym = pd.DataFrame(stime)
     df = pd.concat([tym, bmtrs, v1], axis=1)
-    df.columns=['time','b_m','vel']
+    df.columns=['stime','b_m','vel']
     with open(filename+'.csv','a') as fd:
         writer = csv.writer(fd)
         writer.writerow([i])
@@ -87,6 +109,10 @@ for i in dirlist:
     except (OSError, RuntimeError, TypeError, NameError):
         pass
 
+toc = time.time(); ttime1 = round(toc-tic,4)
+print(' ... I took %s sec. for that :( ...'%ttime1)
+tic = time.time()
+print('\n ==> Now sorting data ...\n')
 #==========================prepare data for analysis=================================
 df = pd.read_csv('grinding_data.csv',skiprows=[0],header=None)
 sumdata = df.iloc[32::34,:]
@@ -138,15 +164,20 @@ bmd10 = df.iloc[307:337]
 bmd10.columns = ['tym','bm','vel']
 bmd10.to_csv('bmd10.csv',index=False)
 
+toc = time.time(); ttime2 = round(toc-tic,4)
+print(' ... I took %s sec. for that :) ...'%ttime2)
+tic = time.time()
+print('\n ==> Now analysing data ...\n')
 #==========================do analysis=======================================
 columns=['r','avSpeed','sdev']
 sumdat = pd.read_csv('summarydata.csv',skiprows=[0],header=None,names=columns)
 x=sumdat['r']; y=sumdat['avSpeed']; dev=sumdat['sdev']
 plt.figure(figsize=(10,6), dpi=500)
-plt.errorbar(x,y,yerr=dev, ecolor='b', capsize=7, mec='blue', color='green', marker='D', linewidth=2, markersize=7)
+plt.errorbar(x,y,yerr=dev, ecolor='b', mec='blue', color='green', marker='D') #, capsize=7, linewidth=2, markersize=7
 plt.xlabel('Active motor ratio', fontdict=font); plt.ylabel('Speed ($\mu m/sec$)', fontdict=font)
 plt.title('Actin Filament Grinding Movement [ATP = 2000, MD = 3000]'); plt.legend(loc='upper left')
-plt.savefig('summary.svg',bbox_inches='tight', format='svg',dip=300)
+plt.savefig('summary.svg',bbox_inches='tight', format='svg',dip=500)
+plt.close()
 
 columns=['tym','bm','vel']
 bmd01 = pd.read_csv('bmd01.csv',skiprows=[0],header=None,names=columns)
@@ -164,6 +195,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd01.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd02 = pd.read_csv('bmd02.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -180,6 +212,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd02.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd03 = pd.read_csv('bmd03.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -196,6 +229,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd03.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd04 = pd.read_csv('bmd04.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -212,6 +246,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd04.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd05 = pd.read_csv('bmd05.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -228,6 +263,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd05.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd06 = pd.read_csv('bmd06.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -244,6 +280,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd06.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd07 = pd.read_csv('bmd07.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -260,6 +297,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd07.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd08 = pd.read_csv('bmd08.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -276,6 +314,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd08.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd09 = pd.read_csv('bmd09.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -292,6 +331,7 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd09.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
 
 bmd10 = pd.read_csv('bmd10.csv',skiprows=[0],header=None,names=columns)
 fig, ax1 = plt.subplots(figsize=(10,6), dpi=500)
@@ -308,3 +348,9 @@ ax2.legend(loc='upper right')
 ax2.tick_params('y', colors='g'); ax2.set_ylim(bottom=0)
 plt.title('Binding motors vs. average speed', fontdict=font)
 plt.savefig('bmd10.svg',bbox_inches='tight', format='svg',dip=300)
+plt.close()
+
+toc = time.time(); ttime3 = round(toc-tic,4)
+print(' ... I took %s sec. for that ;) ...'%ttime3)
+ttime = round(ttime1+ttime2+ttime3,4)
+print('\n ==> I think Im done. %s sec. in total!\n'%ttime)
