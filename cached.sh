2df8112fe0411937f7ac61764f5cf390f806b65f
sirmaxford <sirmaxford@gmail.com>
2021-02-10 17:11:40 +0900

    remove .csv files from remote repo

---
+++ cached.sh
@@ -0,0 +1,12 @@
+# Remove some files from remote repo but not on local repo
+# Sam Macharia
+git rm --cached *.csv # ignore all .csv files
+git rm --cached *.txt
+git rm --cached *.mp4
+git rm --cached *.avi
+git rm --cached *.png
+git rm --cached *.html
+git rm --cached *.vtk
+git rm --cached *.svg
+git rm --cached *.pdf
+git rm --cached *.ipynb_checkpoints
